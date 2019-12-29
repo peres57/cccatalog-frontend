032bc58b9eb0318376ef98ca2719067f6f58575e
peres57 <52344394+peres57@users.noreply.github.com>
2019-12-29 06:12:58 +0300

    Revert "Dismisses mobile keyboard after search submit"

--- src/components/SearchGridForm.vue
+++ src/components/SearchGridForm.vue
@@ -92,7 +92,6 @@ export default {
       e.preventDefault();
       if (this.searchTermsModel) {
         this.$emit('onSearchFormSubmit', { query: { q: this.searchTermsModel }, shouldNavigate: true });
-        this.$refs.search.blur();
       }
     },
     onToggleSearchGridFilter() {
